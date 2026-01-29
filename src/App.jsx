c91f25224885d665a3cb9efd2ef10d859da33008
lasyaJ <lasyareddy.jetty@gmail.com>
2026-01-29 13:34:59 +0530

    show case added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import ProductView from './components/ProductView'
+import ShowCase from './components/ShowCase'
 import gsap from 'gsap';
 import {ScrollTrigger,SplitText} from "gsap/all";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 const App = () => {
   return (
     <main>
       <NavBar/>
       <Hero/>
       <ProductView/>
+      <ShowCase/>
     </main>
 
   )
